cf40b87cfbc398c9d7a8a130d3368c26357f3c11
zakariaFrontDev <zakaria.aitbibote@gmail.com>
2026-06-02 16:37:16 +0100

    returning the svgs file

--- backendapi/index.js
+++ backendapi/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"))
 app.use(express.json());
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "login.html"));
+    res.sendFile(path.join(__dirname, "../public/login.html"));
 });
 app.use(session({
   secret: "secret",
